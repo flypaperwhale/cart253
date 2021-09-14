4ea715a565851730259dfd88d26e0cb394afc186
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-09-14 01:07:15 -0400

    Frankie's Lil Alien: the base is drawn
    my battery is dying so I might not finish it

--- Activities/Draw an Alien/js/script.js
+++ Activities/Draw an Alien/js/script.js
@@ -6,17 +6,31 @@ This program draws an alien, as imagined by Frankie.
 */
 
 /**
-Description of setup
+Attempt to draw an alien with code
 */
 function setup() {
-  createCanvas(500,500);
+  createCanvas(800,800);// a rather large canvas
+  background("#abcdef");// a nice blue indeed
 
-  background(255, 500, 15);
+  // draw the planet land horizon
+  fill(0,0,0); // black
+  rectMode(CORNER);
+  rect(0,649,800,150);
 
-  point(250,250);
+  // draw the alien
+  noStroke();
+  ellipseMode(CENTER);
+  fill(127,127,127); // gray
+  ellipse(400,400,525); // big gray floating head
+  fill(120,120,120); // a darker gray
+  ellipse(400,415,300); // a big gaping hole
+  fill(100,100,100); // an even darker gray
+  ellipse(400,430,120); // deeper into the hole
+  fill(40,40,40); // almost black
+  ellipse(400,444,50); // a throat?
 
-  rectMode(CENTER);
-  rect(250,250,222,222);
+
+
 
 }
 

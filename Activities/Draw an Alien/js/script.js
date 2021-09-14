2c7b7572b7faa7118bcc82e987b98e9523c4303e
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-09-14 01:44:20 -0400

    Frankie's Lil Alien: best I can do for now!
    floating creature with teeth and no eyes

--- Activities/Draw an Alien/js/script.js
+++ Activities/Draw an Alien/js/script.js
@@ -14,8 +14,16 @@ function setup() {
 
   // draw the planet land horizon
   fill(0,0,0); // black
+  triangle(30,650,75,600,120,650); // mountain or pyramid;
+  triangle(700,650,720,620,740,650); // mountain or pyramid;
   rectMode(CORNER);
   rect(0,649,800,150);
+
+  // draw the moon
+  noStroke();
+  fill(255,255,255);
+  ellipseMode(CENTER);
+  ellipse(75,75,40);
 
   // draw the alien
   noStroke();
@@ -28,6 +36,15 @@ function setup() {
   ellipse(400,430,120); // deeper into the hole
   fill(40,40,40); // almost black
   ellipse(400,444,50); // a throat?
+
+  // draw the teeth
+  fill(255,255,255);//white
+  triangle(350,350,360,340,372,372);
+  triangle(230,400,240,390,300,405);
+  triangle(355,550,360,552,363,510);
+  triangle(300,600,305,595,309,599);
+  triangle(445,340,450,395,455,345);
+  triangle(570,430,573,370,579,375);
 
 
 

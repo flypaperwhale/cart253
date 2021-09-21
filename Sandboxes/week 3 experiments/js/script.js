5b925ca04ac49a5dfafb85cbcdbea429a7b6b871
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-09-20 21:49:26 -0400

    learned about debugging variables

--- Sandboxes/week 3 experiments/js/script.js
+++ Sandboxes/week 3 experiments/js/script.js
@@ -10,7 +10,7 @@ let backgroundShade = 0;
 let circleX = -250;
 let circleY = 250;
 let circleSize = 400;
-let circleSpeed = 1;
+let circleSpeed = 2;
 let circleAcceleration = .01;
 
 /**
@@ -37,4 +37,7 @@ function draw() {
     circleSize=circleSize/1.01;
     fill(mouseX*7, mouseY, mouseX/.5);
     ellipse(circleX-4000,circleY,circleSize)
-}
+
+    console.log(`circleX: ${circleX}, circleY: ${circleY}, circleSpeed: ${circleSpeed}`);
+
+  }

0e876df57b29ea1894024719425148fbc5c6a6d2
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-09-20 22:59:26 -0400

    created Moving pictures activity folder

--- Sandboxes/week 3 experiments/js/script.js
+++ Sandboxes/week 3 experiments/js/script.js
@@ -7,7 +7,7 @@ author, and this description to match your project!
 */
 
 let backgroundShade = 0;
-let circleX = -250;
+let circleX = 250;
 let circleY = 250;
 let circleSize = 400;
 let circleSpeed = 2;
@@ -25,19 +25,22 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    let randomNumber = random(-5,5);
     background(backgroundShade);
     ellipseMode(CENTER);
     fill(mouseX*7, mouseY, mouseX/.5);
     circleX=circleX+circleSpeed;
-    circleSpeed = circleSpeed + circleAcceleration;
+    circleSpeed = circleSpeed + randomNumber;
     ellipse(circleX,circleY,circleSize);
     circleSize=circleSize+2.5;
     fill(mouseX*7, mouseY, mouseX/.5);
-    ellipse(circleX-2222, circleY, mouseY);
+    ellipse(circleX, circleY, mouseY);
     circleSize=circleSize/1.01;
     fill(mouseX*7, mouseY, mouseX/.5);
     ellipse(circleX-4000,circleY,circleSize)
 
-    console.log(`circleX: ${circleX}, circleY: ${circleY}, circleSpeed: ${circleSpeed}`);
 
+    //console.log(`circleX: ${circleX}, circleY: ${circleY}, circleSpeed: ${circleSpeed}`);
+
+    console.log(`randomNumber: ${randomNumber}`);
   }

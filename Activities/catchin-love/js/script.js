7d2315e04cc27819237b1a0403e029b7520622a9
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-05 13:12:32 -0400

    A2 : sad ending if statement

--- Activities/catchin-love/js/script.js
+++ Activities/catchin-love/js/script.js
@@ -29,6 +29,8 @@ function setup() {
   // start circles moving in rando
   circle1.vx = random(-circle1.speed,circle1.speed);
   circle2.vx = random(-circle2.speed,circle2.speed);
+  circle1.vy = random(-circle1.speed,circle1.speed);
+  circle2.vy = random(-circle2.speed,circle2.speed);
 }
 
 /**
@@ -36,4 +38,19 @@ Description of draw()
 */
 function draw() {
   background(0);
+  // move circle
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
+
+  // check if the circle have gone offscreen
+  if(circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height){
+    // SAD ENDING
+  }
+
+  // display circles
+  ellipse(circle1.x, circle1.y, circle1.size);
+  ellipse(circle2.x, circle2.y, circle2.size);
 }

4704b65ab8074b22f7d1022391575a1e83d1a3a7
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-05 13:33:32 -0400

    A2 : states added, program complete!

--- Activities/catchin-love/js/script.js
+++ Activities/catchin-love/js/script.js
@@ -18,7 +18,7 @@ let circle2 = {
   speed:3,
 };
 
-let state = `simulation`// can be: title, simulation, sad, love
+let state = `title`// can be: title, simulation, sad, love
 
 /**
 Description of setup
@@ -35,30 +35,49 @@ function draw() {
   background(0);
 
   if (state === `title`){
+    title();
+  }
+  else if (state === `simulation`){
+    simulation();
 
   }
-
-  if (state === `simulation`){
-
+  else if (state === `love`){
+    love();
   }
-
-  if (state === `love`){
-
+  else if (state === `sad`){
+    sad();
   }
-
-  if (state === `sad`){
-    
-  }
-
-  simulation();
-
 }
 
+function title(){
+  push();
+  textSize(64);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text(`LOVE?`,width/2,height/2);
+  pop();
+}
 function simulation(){
   move();
   checkOffscreen();
   checkOverlap();
   display();
+}
+function love(){
+  push();
+  textSize(64);
+  fill(222,100,150);
+  textAlign(CENTER,CENTER);
+  text(`LOVE!!!`,width/2,height/2);
+  pop();
+}
+function sad(){
+  push();
+  textSize(64);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text(`OH noooo :(`,width/2,height/2);
+  pop();
 }
 
 function setupCircles(){
@@ -85,6 +104,7 @@ function checkOffscreen(){
   // check if the circle have gone offscreen
   if(circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height){
     // SAD ENDING
+    state = `sad`;
   }
 }
 
@@ -93,6 +113,7 @@ function checkOverlap(){
   let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
   if (d < circle1.size/2 + circle2.size/2){
     // LOVE ENDING
+    state = `love`;
   }
 }
 
@@ -100,4 +121,10 @@ function display(){
   // display circles
   ellipse(circle1.x, circle1.y, circle1.size);
   ellipse(circle2.x, circle2.y, circle2.size);
+}
+
+function mousePressed() {
+  if (state === `title`){
+    state = `simulation`;
+  }
 }

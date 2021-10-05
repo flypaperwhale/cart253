8edc1c8945681a01dc774c87b817072cfe2bfae4
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-05 13:04:06 -0400

    A5: following pippin, got a black canvas

--- Activities/catchin-love/js/script.js
+++ Activities/catchin-love/js/script.js
@@ -1,33 +1,39 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
+let circle1 = {
+  x:undefined,
+  y:250,
+  size: 100,
+  vx:0,
+  vy:0,
+  speed:3,
+};
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
+let circle2 = {
+  x:undefined,
+  y:250,
+  size: 100,
+  vx:0,
+  vy:0,
+  speed:3,
+};
 
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(500,500);
+  // position circles seperate
+  circle1.x = width/3;
+  circle2.x = 2 * width/3;
+  // start circles moving in rando
+  circle1.vx = random(-circle1.speed,circle1.speed);
+  circle2.vx = random(-circle2.speed,circle2.speed);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
-
+  background(0);
 }

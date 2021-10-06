2c961ad70529b05c00da1a6778bbee5a9557831f
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-06 00:12:29 -0400

    P1 : creating a code to reproduce trees ad infinitum

--- Projects/P1-Forest-Fire/js/script.js
+++ Projects/P1-Forest-Fire/js/script.js
@@ -17,12 +17,27 @@ let bg = {
 
 let pinetree = {
   image:undefined,
+  x:undefined,
+  y:undefined,
+  width:60,
+  height:90,
+  code:`P1`
 };
 let fir = {
   image:undefined,
+  x:undefined,
+  y:undefined,
+  width:60,
+  height:90,
+  code:`F1`
 };
 let birch = {
   image:undefined,
+  x:undefined,
+  y:undefined,
+  width:60,
+  height:90,
+  code:`B1`
 };
 
 
@@ -50,8 +65,43 @@ Description of draw()
 function draw() {
   // draw the background
   background(bg.color.r,bg.color.g,bg.color.b);
-  image(fir.image,250,250,250,250);
+
+  // display a fir (MAESTRO)
+  push();
+  imageMode(CENTER);
+  image(fir.image,300,300,60,90);
+  pop();
+
+  if (isThereNewFir()){
+    establishFirCode();
+    createFir(fir.code);
+  }
+
+
+}
+
+
+function isThereNewFir(){}
+function establishFirCode(){}
 
 
 
+// Functions for creating trees //
+function createFir(fir.code){
+  push();
+  imageMode(CENTER);
+  image(fir.image,fir.x,fir.y,fir.width,fir.height);
+  pop();
+}
+function createBirch(birch.code){
+  push();
+  imageMode(CENTER);
+  image(birch.image,birch.x,birch.y,birch.width,birch.height);
+  pop();
+}
+function createPinetree(pinetree.code){
+  push();
+  imageMode(CENTER);
+  image(pinetree.image,pinetree.x,pinetree.y,pinetree.width,pinetree.height);
+  pop();
 }

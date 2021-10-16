f60b42f6067cffc858ce114f6000d5336d3ad3b4
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-15 20:05:12 -0400

    P1.1 : success, I have one tree standing, time to burn it down

--- Projects/P1-Forest-Fire1/js/script.js
+++ Projects/P1-Forest-Fire1/js/script.js
@@ -8,11 +8,31 @@ author, and this description to match your project!
 
 "use strict";
 
-let forest = [];
-let rows = 5;
-let cols = 5;
+let bg = {
+  r:40,
+  g:130,
+  b:3,
+}
 
-let unit;
+let forest = [
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//A
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//B
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//C
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//D
+  [` `,` `,` `,` `,`P`,` `,` `,` `,` `,` `],//E
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//F
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//G
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//H
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//I
+  [` `,` `,` `,` `,` `,` `,` `,` `,` `,` `],//J
+]
+
+//let forest = [];
+
+//let rows = 10;
+//let cols = 10;
+
+let cellUnit;
 
 let pinetree = {
   image:undefined,
@@ -44,20 +64,27 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(500,500);
   noCursor();
 
+  cellUnit = height / forest.length;
+
+// Initialize the forest
+
+/*
+// USE THIS CODE TO INITIALIZE FOREST + CHECK CELLS
   for (let r = 0; r < rows; r++) {
     forest.push([]);
     for (let c = 0; c < cols; c++) {
       if (random() < 0.15) {
-        forest[r][c] = `F`;
+        forest[r][c] = `?`;
       } else {
         forest[r][c] = ` `;
       }
     }
   }
 
-  unit = height / forest.length;
+  cellUnit = height / forest.length;*/
 }
 
 
@@ -65,9 +92,37 @@ function setup() {
 Description of draw()
 */
 function draw() {
-  createCanvas(500,500);
-  background(0);
+
+  background(bg.r,bg.g,bg.b);
+
+  displayForest();
 
   image(cursor.image,mouseX,mouseY,cursor.width,cursor.height);
 
-}
+};
+
+function displayForest() {
+    for (let y = 0; y < forest.length; y++) {
+    let row = forest[y];
+    for (let x = 0; x < forest[y].length; x++) {
+      push();
+      noFill();
+      stroke(0);
+      rect(x * cellUnit, y * cellUnit, cellUnit, cellUnit);
+      pop();
+      let cell = forest[y][x];
+      if (cell === `P`) {
+        drawGreenCircles(x, y);
+      }
+    }
+  }
+};
+
+function drawGreenCircles(x, y) {
+  push();
+  noStroke();
+  fill(100, 200, 100);
+  ellipseMode(CORNER);
+  ellipse(x * cellUnit, y * cellUnit, cellUnit);
+  pop();
+};

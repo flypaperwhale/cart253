08b67881605dc49930e3ac25e557bba5a4d23d19
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-16 16:01:49 -0400

    P1.2 : Starting from scratch, ditching the grid (for now)

---
+++ Projects/P1.2-Forest-Fire/js/script.js
@@ -0,0 +1,46 @@
+/**
+Forest Fire simulation
+Frankie Latreille
+
+This is a simulation of a forest,
+when it is struck by lightning a tree catches fire.
+(If I can, I will make the trees grow and the fires spread)
+*/
+
+"use strict";
+let forest = [];
+
+let tree = {
+  x:,
+  y:,
+  w:,
+  h:,
+  age:,
+  type:,
+  onFire:,
+}
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+
+
+}

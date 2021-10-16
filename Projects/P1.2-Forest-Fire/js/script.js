cb9e655b8175ce7b3bf1c44f2e9dfd2e6b0b58fa
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-16 16:24:46 -0400

    P1.2 : Setting up function modules and writing notes

--- Projects/P1.2-Forest-Fire/js/script.js
+++ Projects/P1.2-Forest-Fire/js/script.js
@@ -20,6 +20,22 @@ let tree = {
   onFire:,
 }
 
+let fireScape = [];
+
+let fire = {
+  x:,
+  y:,
+  w:,
+  h:,
+  side:,
+}
+
+let cloud = {
+  x:,
+  y:,
+  //....
+}
+
 /**
 Description of preload
 */
@@ -32,6 +48,12 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(400,400);
+
+  initializeForest();
+    createTree(); //when a tree is created during initializtion,
+      treeFactory(); //loop to check that its x,y are not too close to another tree
+      //if it is, assign another random x,y
 
 }
 
@@ -40,7 +62,30 @@ function setup() {
 Description of draw()
 */
 function draw() {
+background(bg.r,bg.g,bg.b);
+
+growingForest();
+  growingTrees();//go through forest array and increase every tree.age++
+  //check for combinations between type + age to switch type (type name = tree type plus tree age)
+  treeReproduction();//when certain tree types reach certain age or burn, a seed is produced
+  check4Seedling();//is there a seed? commence it's aging process, from sprout, to...
+    createTree(); //when a tree is created from reproduction,
+      treeFactory(); //loop to check that its x,y are not too close to another tree
+        //if it is, assign another random x,y
+        //but have the seed fall in the vicinity of the reproducing tree
+
+//when fireState is `on`, fire is displayed
+//tree.onFire is a downwards timer, when it reaches 0, tree.type turns to `Torched`
+fireState();
+
+lightNThunder();
+
+cloud();
 
 
 
+}
+
+function initializeForest(){
+  console.log(`Forest is initialized`);
 }

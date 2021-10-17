d6077aae40a99ff1bb2783fe09f38fdeb912f25b
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-17 14:06:40 -0400

    P1.2 : got the cloud floating over the tree and click for fire creates great effect

--- scrapbox2/js/script.js
+++ scrapbox2/js/script.js
@@ -18,12 +18,39 @@ let fire = {
   side:1, //or side 2
 }
 
-let cloud = {
+/*let cloud = {
   image: undefined,
   x:30,
   y:30,
   w:60,
   h:40,
+}
+*/
+
+let lightning = {
+  x:0,
+  y:0,
+  color:255,
+  alpha:0,
+}
+
+let cloud = {
+  image:undefined,
+  x:50,
+  y:60,
+  speed:1,
+  vx:0,
+  vy:0,
+  width:65,
+  height:50,
+  clicked:undefined,
+}
+let cursor = {
+  image:undefined,
+  x:0,
+  y:0,
+  width:45,
+  height:45,
 }
 
 let fireIMG1;
@@ -41,12 +68,19 @@ function preload() {
   cloud.image = loadImage('assets/images/cloud.png');
 
   thunderSFX = loadSound(`assets/sounds/THUND.WAV`);
+
+  cursor.image = loadImage('assets/images/Godly-user.png');
 }
 
 function setup(){
   createCanvas(400,400);
   frameRate(10);
 
+  // intialize cloud position and velocity
+    cloud.y = random(0,height);
+    cloud.vx = cloud.vx + cloud.speed;
+
+    noCursor();
 }
 
 
@@ -74,6 +108,28 @@ function draw(){
     displayFire();
   }
 
+  lightFilter();
+
+  // Clowns movement //
+  // clown 1
+  cloud.x = cloud.x + cloud.vx;
+  cloud.y = cloud.y + cloud.vy;
+  // clowns reapear at the left side when arriving at the right side
+  if (cloud.x > width){
+    cloud.x = 0;
+    cloud.y = random(0,height);}
+
+  push();
+  imageMode(CENTER);
+  image(cloud.image,cloud.x,cloud.y,cloud.width,cloud.height);
+  pop();
+
+  cursor.x = mouseX;
+  cursor.y = mouseY;
+  image(cursor.image,cursor.x,cursor.y,cursor.width,cursor.height);
+
+  lightNThunder();
+
 }
 
 function displayFire(){
@@ -83,3 +139,42 @@ function displayFire(){
 function mousePressed(){
   onFire = 1;
 }
+
+function lightFilter(){
+  console.log(`alpha1=${lightning.alpha}`);
+  push();
+  noStroke();
+  fill(lightning.color,lightning.alpha);
+  rectMode(CENTER);
+  rect(lightning.x,lightning.y,canvas.width,canvas.height);
+  pop();
+  console.log(`alpha2=${lightning.alpha}`);
+}
+
+function MouseOverCloud(){
+  let d = dist(cursor.x, cursor.y, cloud.x, cloud.y);
+  if (d < cursor.width/2 + cloud.width/2){
+    return true;}
+  else {
+    return false
+  }
+  }
+
+function mouseClicked(){
+  if (MouseOverCloud()){
+    cloud.clicked=true;
+    if (cloud.clicked === true){
+
+      thunderSFX.play();
+      lightning.alpha = 255;
+      cloud.clicked=false;
+    }
+  }
+};
+
+function lightNThunder(){
+    console.log(`you've made it in lightnthunder`);
+    lightning.alpha = random(1-255);
+
+  }
+  //console.log(`mouseclicked = ${mouseClicked()} and frame = ${frame} and lightning.alpha = ${lightning.alpha}`);

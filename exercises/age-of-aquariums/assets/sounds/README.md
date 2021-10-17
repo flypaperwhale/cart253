052deaad2183d0fe665d8406988f3ab64c0372ec
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-17 00:50:48 -0400

    E4 : added wav sounds, played with fish colors.
    going to try to make it so when orbs are clicked they freeze state and make a sound. find the spirit with the secret!

--- exercises/age-of-aquariums/assets/sounds/README.md
+++ exercises/age-of-aquariums/assets/sounds/README.md
@@ -1,3 +1,9 @@
 # Sounds folder
 
 This is the folder where you would keep all the sound files associated with your project, such as `.mp3`, `.wav`, and other formats. An example sound, `bark.wav`, is here in case you need something to play around with.
+
+magic spirit - mbutterfly - starmen.net/mother2/soundfx/mbutterfly.wav
+
+dud spirit - mysterious - starmen.net/mother2/soundfx/mysterious.wav
+
+wonky spirit -smrpg_ghost - http://www.superluigibros.com/downloads/sounds/SNES/SMRPG/wav/smrpg_ghost.wav
--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -7,8 +7,11 @@ Behold! It looks as though I am creating life out of code!
 
 "use strict";
 
+let round = 0;
+let magicNum = 2;
+
 let school = [];
-let schoolSize = 5;
+let schoolSize = 0;
 
 let cursor = {
   image:undefined,
@@ -31,23 +34,29 @@ Description of setup
 */
 function setup() {
   createCanvas(600,600);
-
   noCursor();
-  
+  schoolSize = random(1,12);
 //create fish
 for (let i = 0; i < schoolSize; i++){
-  school[i] = createFish(random(0,width), random(0,height));
-  }
+  school[i] = createFish(random(0,width), random(0,height), random(0,255),);
+  colorizeFish(school[i]);
+}
 }
 
 function createFish(x,y){
   let fish = {
     x: x,
     y: y,
-    size:50,
+    size:100,
     vx:0,
     vy:0,
     speed:2,
+    color:{
+      r:0,
+      g:0,
+      b:0,
+    },
+    song:undefined,
   };
   return fish;
 }
@@ -56,15 +65,40 @@ function createFish(x,y){
 Description of draw()
 */
 function draw() {
-  background(0);
+  background(100,180,225);
 
   for (let i = 0; i < school.length; i++){
+
     moveFish(school[i]);
     displayFish(school[i]);
   }
+
   drawUser();
+
 }
 
+function colorizeFish(fish){
+  round = round + 1 ;
+  if (round === 1){
+    fish.color.r =0;
+    fish.color.b =200;
+    fish.color.g =150;
+    fish.song = x;
+}
+  if (round === 2){
+    fish.color.r =100;
+    fish.color.b =255;
+    fish.color.g =0;
+    fish.song = y;
+  }
+  if (round === 3){
+    fish.color.r =100;
+    fish.color.b =0;
+    fish.color.g =255;
+    fish.song = z;
+    round = 1;
+  }
+}
 //move fish()
 // choose whether provided fish changes direction and moves it
 function moveFish(fish){
@@ -82,12 +116,11 @@ function moveFish(fish){
   fish.y = constrain(fish.y, 0, height);
 }
 
-
 //display Fish
 //displays the provided fish on the canvas
 function displayFish(fish){
   push();
-  fill(145,225,100);
+  fill(fish.color.r,fish.color.g,fish.color.b);
   noStroke();
   ellipse(fish.x,fish.y,fish.size);
   pop();

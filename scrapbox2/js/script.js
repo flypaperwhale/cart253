d2e85e5f68f7930285859e3ba530b2dc9c6f6734
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-17 13:19:57 -0400

    P1.2 : using scrapbox, YAY finally got tree to burn on click!

--- scrapbox2/js/script.js
+++ scrapbox2/js/script.js
@@ -1,9 +1,9 @@
-
+"use strict";
 
 let tree = {
   image: undefined,
-  x:0,
-  y:0,
+  x:200,
+  y:200,
   w:50,
   h:60,
   onFire:0,
@@ -26,11 +26,57 @@ let cloud = {
   h:40,
 }
 
-function preload(){
+let fireIMG1;
+let fireIMG2;
+let thunderSFX;
+
+let onFire=0;
+
+function preload() {
+  tree.image = loadImage('assets/images/Fir2.png');
+
+  fireIMG1 = loadImage('assets/images/fire1.png');
+  fireIMG2 = loadImage('assets/images/fire2.png'); // <--- FLICKER
+
+  cloud.image = loadImage('assets/images/cloud.png');
+
+  thunderSFX = loadSound(`assets/sounds/THUND.WAV`);
+}
+
+function setup(){
+  createCanvas(400,400);
 
 }
 
+
 function draw(){
+  background(0);
+
+  tree.x = width/2;
+  tree.y = height/2;
+
+  image(tree.image,tree.x,tree.y,tree.w,tree.h);
+  image(cloud.image,cloud.x,cloud.y,cloud.w,cloud.h);
 
 
+
+  if (fire.side === 1){
+    fire.image = fireIMG1;
+  }
+  else if (fire.side === 2){
+    fire.image = fireIMG2;
+  }
+
+  if (onFire===1){
+    displayFire();
+  }
+
+}
+
+function displayFire(){
+  image(fire.image,tree.x,tree.y,fire.w,fire.h);
+}
+
+function mousePressed(){
+  onFire = 1;
 }

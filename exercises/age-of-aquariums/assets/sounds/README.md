a950190d5505b4bf0ef61fb5dcbd3dd92ea8d5a5
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-10-18 20:20:10 -0400

    e4: got states in, gotta code what happens, touch up program, fix layout + comment

--- exercises/age-of-aquariums/assets/sounds/README.md
+++ exercises/age-of-aquariums/assets/sounds/README.md
@@ -2,8 +2,13 @@
 
 This is the folder where you would keep all the sound files associated with your project, such as `.mp3`, `.wav`, and other formats. An example sound, `bark.wav`, is here in case you need something to play around with.
 
-magic spirit - mbutterfly - starmen.net/mother2/soundfx/mbutterfly.wav
+magic spirit - sosp6
 
 dud spirit - mysterious - starmen.net/mother2/soundfx/mysterious.wav
 
 wonky spirit -smrpg_ghost - http://www.superluigibros.com/downloads/sounds/SNES/SMRPG/wav/smrpg_ghost.wav
+
+
+title song - smallbowl_ext_sound_1 - www.cipoo.net/downloads/sounds/smallbowl_ext_sound_1.wav
+
+unused - mbutterfly - starmen.net/mother2/soundfx/mbutterfly.wav
--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -7,17 +7,21 @@ Behold! It looks as though I am creating life out of code!
 
 "use strict";
 
+let timer = 0;
 let state = `title`; //can be title, game, playSound, ending1, ending2
-
-let x;
-let y;
-let z;
-
+let playTitleSong = 1;
+let titleSong;
+let dudSong;
+let wonkySong;
+let secretSong;
+let clickFish;
 let round = 0;
 let magicNum = 2;
 
 let school = [];
 let schoolSize = 0;
+
+let itIs;
 
 let cursor = {
   image:undefined,
@@ -32,6 +36,13 @@ Description of preload
 */
 function preload() {
   cursor.image = loadImage('assets/images/hermes-staff.png');
+
+  titleSong = loadSound('assets/sounds/title.wav');
+  dudSong = loadSound('assets/sounds/dud-spirit.wav');
+  wonkySong = loadSound('assets/sounds/wonky-spirit.wav');
+  secretSong = loadSound('assets/sounds/magic-spirit.wav');
+
+
 }
 
 
@@ -41,7 +52,7 @@ Description of setup
 function setup() {
   createCanvas(600,600);
   noCursor();
-  schoolSize = random(1,12);
+  schoolSize = random(3,9);
 //create fish
 for (let i = 0; i < schoolSize; i++){
   school[i] = createFish(random(0,width), random(0,height), random(0,255),);
@@ -53,7 +64,7 @@ function createFish(x,y){
   let fish = {
     x: x,
     y: y,
-    size:100,
+    size:75,
     vx:0,
     vy:0,
     speed:2,
@@ -74,41 +85,71 @@ function draw() {
   background(100,180,225);
 
 if (state === `title`){
-  push();
-  textAlign(CENTER,CENTER);
-  text(`You've entered a magical plane`, width/2, height/2);
-  pop();
+    if (playTitleSong === 1){
+    titleSong.play();
+    playTitleSong =0;}
+    push();
+    imageMode(CENTER);
+    image(cursor.image,width/2,height/2-120,cursor.width,cursor.height);
+    pop();
+    push();
+    textAlign(CENTER,CENTER);
+    text(`You've entered a magical plane`, width/2, height/2);
+    pop();
+    timer++;
+    if (timer === 300){
+      state = `spirits state`;
+    }
 }
 
+if (state === `spirits state`){
   for (let i = 0; i < school.length; i++){
+
 
     moveFish(school[i]);
     displayFish(school[i]);
+
+    drawUser();
+
+    if (clickFish===3){
+      state = `secret eludes`;
+    }
   }
+}
 
-  drawUser();
+if (state === `secret eludes`){
 
+}
+
+if (state === `caughtSecret`){
+
+}
+
+if (state === `ending`){
+  
+}
 }
 
 function colorizeFish(fish){
   round = round + 1 ;
   if (round === 1){
+    fish.secret=1;
     fish.color.r =0;
     fish.color.b =200;
     fish.color.g =150;
-    fish.song = x;
+    fish.song = secretSong;
 }
   if (round === 2){
     fish.color.r =100;
     fish.color.b =255;
     fish.color.g =0;
-    fish.song = y;
+    fish.song = dudSong;
   }
   if (round === 3){
     fish.color.r =100;
     fish.color.b =0;
     fish.color.g =255;
-    fish.song = z;
+    fish.song = wonkySong;
     round = 1;
   }
 }
@@ -144,4 +185,26 @@ function drawUser(){
   imageMode(CENTER);
   image(cursor.image,mouseX,mouseY,cursor.width,cursor.height);
   pop();
+}
+
+function mouseClicked(){
+  clickFish = clickFish++;
+  if (state === `title`){
+    console.log(`do nothing`);
+  }
+  else if (state === `spirits state`){
+    for(let i = 0; i<school.length;i++){
+      let fish = school[i];
+      let d = dist(mouseX, mouseY-70, fish.x, fish.y);
+      if (d < cursor.width/5 + fish.size/2){
+        console.log(`mouse is over`);
+        fish.color.r=fish.color.r+30;
+        fish.color.b=fish.color.b+70;
+        fish.song.play();
+        if (fish.secret===1){
+          state = `caughtSecret`;
+        }
+      }
+    }
+  }
 }

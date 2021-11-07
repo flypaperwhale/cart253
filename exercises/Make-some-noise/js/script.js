a801c91f6a385edf70e430af72c3443638abf13c
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-11-06 21:20:11 -0400

    E7 : added assets. got a background image + a rectangle.

--- exercises/Make-some-noise/js/script.js
+++ exercises/Make-some-noise/js/script.js
@@ -1,18 +1,29 @@
 /**
-Title of Project
-Author Name
+Sky Glow audio-visuals
+Frankie Latreille
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This project is more an aesthetic research.
+I will use images and shapes to create a background,
+and I will use states to have different audio-visual effects
+there will also be noisy buttons to navigate the states
 */
 
 "use strict";
+
+let starsBackground;
+
+let sunsetStarsIntro;
+let backgroundMusic;
+
 
 
 /**
 Description of preload
 */
 function preload() {
+  starsBackground = loadImage('assets/images/starnight.jpg');
+  sunsetStarsIntro = loadSound(`assets/sounds/intro-constellation.mp3`);
+  backgroundMusic = loadSound(`assets/sounds/skyglowbgmusic.mp3`);
 
 }
 
@@ -21,7 +32,19 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(600,800);
 
+  push();
+  imageMode(CENTER);
+  image(starsBackground,width/2, height/2,600,800);
+  pop();
+
+  push();
+  noStroke();
+  fill(30,75,40);
+  rectMode(CENTER);
+  rect(width/2,height,600,800);
+  pop();
 }
 
 
@@ -29,5 +52,7 @@ function setup() {
 Description of draw()
 */
 function draw() {
+
+
 
 }

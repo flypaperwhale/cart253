bf35d8b60d1f7bbe3f7a6aa8e643bcce31c5b282
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2021-12-07 23:24:24 -0500

    P2 : picking up items works, they get pushed to inventory, but the textbox just flashes

--- Projects/P2/js/SimulationState.js
+++ Projects/P2/js/SimulationState.js
@@ -16,22 +16,22 @@ class SimulationState extends State {
 
   createItems() {
     this.slingshot = new Slingshot(itemImagesList[0]);
-    this.player.inventory.push(this.slingshot); //
+    this.simulationItemList.push(this.slingshot); //
     //
     this.ham = new Ham(itemImagesList[1]);
-    this.player.inventory.push(this.ham); //
+    this.simulationItemList.push(this.ham); //
     //
     this.bigBone = new BigBone(itemImagesList[2]);
-    this.player.inventory.push(this.bigBone); //
+    this.simulationItemList.push(this.bigBone); //
     //
     this.frog = new Frog(itemImagesList[3]);
-    this.player.inventory.push(this.frog); //
+    this.simulationItemList.push(this.frog); //
     //
     this.wrench = new Wrench(itemImagesList[4]);
-    this.player.inventory.push(this.wrench); //
+    this.simulationItemList.push(this.wrench); //
     //
     this.injunction = new Injunction(itemImagesList[5]);
-    this.player.inventory.push(this.injunction); //
+    this.simulationItemList.push(this.injunction); //
     //
   }
 
@@ -61,28 +61,33 @@ class SimulationState extends State {
     this.simulationNPCList.push(this.streetLightC); // √
     this.jade = new Jade(); // mapC
     this.simulationNPCList.push(this.jade); // !!!
-
   }
 
-
   update() {
-
     this.display(); // simulation state display method
 
-    this.player.display(); // display the player avatar
+    // Check if player is paused (when textBubble appears)
+    if (this.player.isPaused === true) {
+      // if player is paused
+      this.player.vx = 0; // turn player velocity to 0
+      this.player.vy = 0;
+    } else if (this.player.isPaused === false) {
+      // if player is not paused
+      this.player.handleInput(); // handle player input: up, down, left, right, w,s,a,d,
+      this.player.move(); // change the player avatar's position
+    }
 
     // go through the NPC array to display each NPC (according to the map player is on)
     // also, check if player is colliding and update the NPC's data if need be (from click & trade)
-    for (let i = 0; i < this.simulationNPCList.length; i++) {
-      this.simulationNPCList[i].display();
-      this.simulationNPCList[i].playerCollisionCheck(
+    for (let i = 0; i < this.simulationNPCList.length; i++) { // Look through NPC List
+      this.simulationNPCList[i].display(); // display every NPC
+      this.simulationNPCList[i].playerCollisionCheck( //NPC/Player Collision check
         this.player.x,
         this.player.y,
         this.player.size
       );
 
       if (this.simulationNPCList[i].isTriggered === true) {
-        // this should only happen if player is colliding w npc
         // if when player is colliding with npc player also presses spacebar
         this.player.paused(); // player avatar movement becomes paused
         // SimulationState desiredItem is temporarily the clicked npc's desired item (if any)
@@ -99,69 +104,76 @@ class SimulationState extends State {
 
         // this is a text assigning machine //
         if (this.simulationNPCList[i].textNo === 0) {
-          this.textBubble = new TextBubble(`${this.simulationNPCList[i].texts[0]}`);
+          this.textBubble = new TextBubble(
+            `${this.simulationNPCList[i].texts[0]}`
+          );
         } else if (this.simulationNPCList[i].textNo === 1) {
-          this.textBubble = new TextBubble(`${this.simulationNPCList[i].texts[1]}`);
+          this.textBubble = new TextBubble(
+            `${this.simulationNPCList[i].texts[1]}`
+          );
           this.player.inventory.shift();
           let item = new Item(0, 0, `Big bone`); // this is problematic
           this.player.inventory.push(item);
         } else if (this.simulationNPCList[i].textNo === 2) {
-          this.textBubble = new TextBubble(`${this.simulationNPCList[i].texts[2]}`);
+          this.textBubble = new TextBubble(
+            `${this.simulationNPCList[i].texts[2]}`
+          );
         }
 
         if (this.player.tradeHappens === true) {
-          this.simulationNPCList[i].textNo = 2;
+          this.simulationNPCList[i].textNo = 1;
           this.player.tradeHappens = false;
         }
         if (this.simulationNPCList[i].tradeSucceeded === true) {
-          this.simulationNPCList[i].textNo = 3;
+          this.simulationNPCList[i].textNo = 2;
         }
-
+// Display assigned text
         this.textBubble.display();
       }
     }
 
-    for (let i = 0; i < this.player.inventory.length; i++) { // look through the inventory
-      this.player.inventory[i].display();
+    for (let i = 0; i < this.simulationItemList.length; i++) {
+      // look through the inventory
+      this.simulationItemList[i].display();
       // display inventory items (can be used in inventory display, x y determined right before display)
-      this.player.inventory[i].playerCollisionCheck(this.player.x,this.player.y,this.player.size);
+      this.simulationItemList[i].playerCollisionCheck(
+        this.player.x,
+        this.player.y,
+        this.player.size
+      );
       // check for player collision with each item
 
-      if (this.player.inventory[i].playerCollided === true) { // if the player has collided with an item
-      this.player.inventory[i].isOnMap = false; // item is declared off of the map
-      if (this.player.inventory[i].isOnMap === false) { // when item is declared off of the map
-        this.player.inventory[i].x = undefined; // item x coordinate becomes undefined, vanishing item
-      }
-      this.player.paused(); // this collision causes the game to pause
-      this.eventSwitch = constrain(this.eventSwitch, 0, 1);
-      if (this.eventSwitch === 0) {
-        this.player.inventory.push(this.player.inventory[i]); // ### this is a silly loop
-        // ### becuase I am using inventory in debug state!
-        console.log(`you just pushed an item in inventory. inv lgt now ${this.player.inventory.length}`);
-        //##
-        this.textBubble = new TextBubble(
-          `You just picked up ${this.player.inventory[0]}`
-        );
-      }
-      this.eventSwitch++;
-      this.textBubble.display();
-    }
-  }
+// Player collision with items //
+      if (this.simulationItemList[i].playerColliding === true) {
+        // if the player has collided with an item
+        this.simulationItemList[i].isPicked = true;
+        this.simulationItemList[i].isOnMap = false; // item is declared off of the map
+        if (this.simulationItemList[i].isOnMap === false) {
+        // when item is declared off of the map
+          this.simulationItemList[i].x = undefined; // item x coordinate becomes undefined,
+          // vanishing item
+        }
 
+      //  this.eventSwitch = constrain(this.eventSwitch, 0, 1); // switch can be 0 or 1
+      //  if (this.eventSwitch === 0) { // when switch is initialized (at 0)
+          this.player.inventory.push(this.simulationItemList[i]); // item is pushed in inventory
+          console.log(
+            `you just pushed an item ${this.simulationItemList[i].name} in inventory. inv lgt now ${this.player.inventory.length}`
+          );
+          this.textBubble = new TextBubble( // text is assigned to textbubble
+            `You just picked up ${this.player.inventory[0].name}`
+          );
+          this.textBubble.display();
+          this.player.paused(); // this collision causes the game to pause
+        }
+      //  this.eventSwitch++;
 
-    // Check if player is paused (when textBubble appears)
-    if (this.player.isPaused === true) {
-      // if player is paused
-      this.player.vx = 0; // turn player velocity to 0
-      this.player.vy = 0;
-    } else if (this.player.isPaused === false) {
-      // if player is not paused
-      this.player.handleInput(); // handle player input: up, down, left, right, w,s,a,d,
-      this.player.move(); // change the player avatar's position
+      }
+    
+
+      this.player.display(); // display the player avatar
     }
 
-    this.player.display(); // display the player avatar
-  }
 
   display() {
     background(0);
@@ -180,14 +192,24 @@ class SimulationState extends State {
 
     if (keyCode === 32) {
       console.log(`SPACE`);
+      for (let i = 0; i < this.simulationItemList.length; i++){
+        if (this.simulationItemList[i].isPicked === true && this.player.isPaused === true){
+          this.textBubble.break();
+          this.simulationNPCList[i].isTriggered = false;
+          this.player.isPaused = false;
+          this.simulationNPCList[i].playerColliding = false;
+          if (this.simulationNPCList[i].textNo === 2) {
+            this.simulationNPCList[i].tradeSucceeded = true;
+          }
+        }
+      }
+
       for (let i = 0; i < this.simulationNPCList.length; i++) {
         if (this.simulationNPCList[i].playerColliding === true) {
           this.simulationNPCList[i].isTriggered = true;
         }
 
-        if ((this.simulationNPCList[i].isTriggered === true && this.player.isPaused === true) ||
-          (this.simulationNPCList[i].playerColliding === true && this.player.isPaused === true)
-        ) {
+        if (this.simulationNPCList[i].isTriggered === true && this.player.isPaused === true){
           this.textBubble.break();
           this.simulationNPCList[i].isTriggered = false;
           this.player.isPaused = false;
